27729c3668f33698434214e1fcab2d85aa4e04d0
GabeDebug <gabrielmessiasc0d3@gmail.com>
2026-04-13 00:24:43 -0700

    Criação do método Role

--- Models/Role.cs
+++ Models/Role.cs
@@ -4,10 +4,6 @@ namespace Blog.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Email { get; set; }
-        public string PasswordHash { get; set; }
-        public string Bio { get; set; }
-        public string ImageUrl { get; set; }
         public string Slug { get; set; }
     }
 }
---
+++ Models/User.cs
@@ -0,0 +1,13 @@
+namespace Blog.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public string Bio { get; set; }
+        public string ImageUrl { get; set; }
+        public string Slug { get; set; }
+    }
+}
